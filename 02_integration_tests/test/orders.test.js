1aad5a78097e76ae99b8b0e9269538cc16c0787b
Tzuzul <mail@tzuzulcode.com>
2022-09-08 12:14:15 -0500

    Eslint y environments

--- 02_integration_tests/test/orders.test.js
+++ 02_integration_tests/test/orders.test.js
@@ -4,25 +4,25 @@ const expect = Chai.expect
 const OrdersService = require("../services/orders")
 const OrdersController = require("../controllers/orders")
 
-describe("Orders",()=>{
+describe("Orders", () => {
 
-    it("Should calculate the total",()=>{
+    it("Should calculate the total", () => {
         // Integración
         const total = OrdersController.calculateTotal("abc123")
 
-        const expectedTotal = 45.67+566.33
+        const expectedTotal = 45.67 + 566.33
 
         expect(total).to.be.a("object")
         expect(total.total).to.be.a("number")
         expect(total.total).to.be.equals(expectedTotal)
         expect(total.totalWithTax).to.be.a("number")
-        expect(total.totalWithTax).to.be.equals(expectedTotal+expectedTotal*0.05)
+        expect(total.totalWithTax).to.be.equals(expectedTotal + expectedTotal * 0.05)
     })
 
-    it("Should calculate the sum of the products",()=>{
-        let total = OrdersService.getTotal([{name:"televisor",price:45.67},{name:"computadora",price:566.33}])
+    it("Should calculate the sum of the products", () => {
+        let total = OrdersService.getTotal([{ name: "televisor", price: 45.67 }, { name: "computadora", price: 566.33 }])
 
         expect(total).to.be.a("number")
-        expect(total).to.be.equals(45.67+566.33)
+        expect(total).to.be.equals(45.67 + 566.33)
     })
 })
